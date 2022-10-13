1b0c75093eceabb4f6f37d0662346e7d8841568d
prashant6311 <prashantppatil761@gmail.com>
2022-10-13 20:29:29 +0530

    Welcome Snake And Ladder Program

---
+++ snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package snakeandladder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Snake and Ladder Program on Master Branch");
+
+	}
+
+}
