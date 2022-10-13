a5f3b3964083d2c4d295e2385751a067bd7a6ad5
prashant6311 <prashantppatil761@gmail.com>
2022-10-13 20:39:14 +0530

    Roll Dice

--- snakeandladder/SnakeAndLadder.java
+++ snakeandladder/SnakeAndLadder.java
@@ -1,11 +1,17 @@
 package snakeandladder;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Snake and Ladder Program on Master Branch");
 		System.out.println("Inintial position of the player1 is at 0");
+		
+		Random random = new Random();
+        int dice= random.nextInt(6)+1;
+        System.out.println("Player is rolling dice and get "+dice);
 	}
 
 }
