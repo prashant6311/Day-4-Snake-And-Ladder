21a5f65873189bc49f3800dc211004471f3664a5
prashant6311 <prashantppatil761@gmail.com>
2022-10-13 20:36:09 +0530

    start program

--- snakeandladder/SnakeAndLadder.java
+++ snakeandladder/SnakeAndLadder.java
@@ -5,7 +5,7 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Snake and Ladder Program on Master Branch");
-
+		System.out.println("Inintial position of the player1 is at 0");
 	}
 
 }
